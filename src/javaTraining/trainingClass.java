6b08870eac389ff506fcca833d82be918d391d68
unknown <kazzow35@gmail.com>
2020-02-02 10:37:40 -0500

    first commit

---
+++ src/javaTraining/trainingClass.java
@@ -0,0 +1,47 @@
+package javaTraining;
+
+public class trainingClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	// multiple Statement
+		
+		String lastName;
+		
+		lastName= "Khalafallah";
+		
+		System.out.println(lastName);
+	
+	////////////
+		
+	// Single Statement
+		
+		String firstName="Mohmaed";
+		
+		System.out.println(firstName);
+		
+		
+	//////////
+		
+	// Concatenation
+		
+		String carModel= "Honda";
+		
+		System.out.println("My car Model is " + carModel);
+		
+	////////
+		
+		String myName= "Mohamed";
+		
+		String address= "123 abc, Ashburn";
+		
+		String appartment= "201";
+		
+		System.out.println("My name is " + myName + "," + " & I live in\n" + address+ " Apt " + appartment );
+		
+		
+		
+	}
+
+}
